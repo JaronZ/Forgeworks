9c63be8eeb9679e0d9f4e5c7354575970e9fe9e4
Jaron Zijlstra <support@jaronline.dev>
2025-05-28 23:33:36 +0200

    feat: setup forgeworks modpack

---
+++ packages/forgeworks/src/lib/event/server/recipes.ts
@@ -0,0 +1,108 @@
+import {
+	LOG_SUFFIX,
+	STRIPPED_PREFIX,
+	PLANKS_SUFFIX,
+	WOOD_SUFFIX
+} from "../../util/name";
+import { sawmillRecipe } from "../../util/immersive_engineering";
+
+ServerEvents.recipes((event) => {
+	function registerLogSawmillRecipe(log, options): void {
+		sawmillRecipe(
+			event,
+			Object.assign(
+				{
+					input: `${log}${LOG_SUFFIX}`,
+					stripped: `${STRIPPED_PREFIX}${log}${LOG_SUFFIX}`,
+					output: `${log}${PLANKS_SUFFIX}`
+				},
+				options
+			)
+		);
+	}
+
+	function registerStrippedLogSawmillRecipe(log, options): void {
+		sawmillRecipe(
+			event,
+			Object.assign(
+				{
+					input: `${STRIPPED_PREFIX}${log}${LOG_SUFFIX}`,
+					output: `${log}${PLANKS_SUFFIX}`
+				},
+				options
+			)
+		);
+	}
+
+	function registerWoodSawmillRecipe(log, options): void {
+		sawmillRecipe(
+			event,
+			Object.assign(
+				{
+					input: `${log}${WOOD_SUFFIX}`,
+					stripped: `${STRIPPED_PREFIX}${log}${WOOD_SUFFIX}`,
+					output: `${log}${PLANKS_SUFFIX}`
+				},
+				options
+			)
+		);
+	}
+
+	function registerStrippedWoodSawmillRecipe(log, options): void {
+		sawmillRecipe(
+			event,
+			Object.assign(
+				{
+					input: `${STRIPPED_PREFIX}${log}${WOOD_SUFFIX}`,
+					output: `${log}${PLANKS_SUFFIX}`
+				},
+				options
+			)
+		);
+	}
+
+	function registerSawmillRecipes(logList, options): void {
+		if (!options) options = {};
+
+		logList.forEach((log) => {
+			registerLogSawmillRecipe(log, options);
+			registerStrippedLogSawmillRecipe(log, options);
+			registerWoodSawmillRecipe(log, options);
+			registerStrippedWoodSawmillRecipe(log, options);
+		});
+	}
+
+	registerSawmillRecipes(
+		[
+			"fir",
+			"magic",
+			"pine",
+			"maple",
+			"redwood",
+			"mahogany",
+			"jacaranda",
+			"palm",
+			"willow",
+			"dead",
+			"umbran",
+			"hellbark",
+			"empyreal"
+		],
+		{
+			namespace: "biomesoplenty"
+		}
+	);
+	registerSawmillRecipes(["fruit", "cinnamon"], {
+		namespace: "extradelight"
+	});
+
+	//Cinnamon Wood
+	registerWoodSawmillRecipe("cinnamon", {
+		namespace: "extradelight",
+		stripped: "strippedcinnamon_wood"
+	});
+	registerStrippedWoodSawmillRecipe("cinnamon", {
+		namespace: "extradelight",
+		input: "strippedcinnamon_wood"
+	});
+});
