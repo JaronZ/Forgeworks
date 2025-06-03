97ab4fdd656ac512de67848d004ea38ef778c9ff
LiekevanKempen <LIL.vanKempen@student.han.nl>
2025-06-03 20:18:49 +0200

    feat: added folder and changed rewards in minigames

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,3 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace(["./packages/forgeworks/vitest.config.ts"]);
+export default defineWorkspace(["./packages/forgeworks/vitest.config.ts", "./packages/minigames/vitest.config.ts"]);
